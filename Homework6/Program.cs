885060a0f3ee9f53e2d4f26b4529a8f3ebc56ed1
trossman <zacknafien2000@yahoo.com>
2016-07-31 12:34:23 -0500

    Homework6

---
+++ Homework6/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework6
+{
+    class Television
+    {
+        private int channel = 0;
+        private int volume = 0;
+        private bool isOn = false;
+
+        public bool IsOn
+        {
+            get { return isOn; }
+            set { isOn = value; }
+        }
+
+        public int Volume
+        {
+            get { return volume; }
+            set
+            {
+                switch (value > volume)
+                {
+                    case true:
+                        if (volume < 100)
+                        {
+                            volume = value;
+                        }
+                        break;
+
+                    default:
+                        if (volume > 0)
+                        {
+                            volume = value;
+                        }
+                        break;
+                }
+            }
+        }
+
+        public int Channel
+        {
+            get{ return channel; }
+            set
+            {
+                switch (channel > 0 || channel < 50)
+                {
+                    case true:
+                        channel = value;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    class TestTV
+    {
+        static void Main()
+        {
+            Television tv = new Television();
+
+            if (!tv.IsOn)
+            {
+                tv.IsOn = true;
+            }
+
+            tv.Channel = 3;
+
+            tv.Volume++;
+            tv.Volume++;
+            tv.Volume++;
+            tv.Volume++;
+
+            tv.IsOn = false;
+        }
+    }
+}
